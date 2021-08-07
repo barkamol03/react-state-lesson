ed9dd06295cc41b1ede66ecd0e65cc1d33948c42
Barkamol <barkamol@gmail.com>
2021-08-07 22:53:30 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './app.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            title: 'Webbrain Academy',
+            count: 0,
+        } 
+    } 
+    render(){
+        const change =()=> {
+            this.setState({title: 'IT Academy', count: 1}) 
+        } 
+
+        let plus =()=>{
+            if (this.state.count < 10) this.setState({count: this.state.count +1}) 
+            if (this.state.count === 0) this.setState({count: 0})
+        }
+
+        let minus =()=>{
+            if (this.state.count > 0) {
+                this.setState({count: this.state.count -1})
+            }
+        } 
+
+        let titlechange =(e)=>{
+            console.log(e.target.value);
+            this.setState({title: e.target.value}) 
+        }
+
+        return <div>
+            <h1>{this.state.title} {this.state.count}</h1> 
+            <input type='text' onChange={titlechange}/>
+            <button onClick={change}>Change</button>
+            <button onClick={plus}>+</button>
+            <button onClick={minus}>-</button> 
+        </div>
+    }
 }
 
-export default App;
+export default App 
